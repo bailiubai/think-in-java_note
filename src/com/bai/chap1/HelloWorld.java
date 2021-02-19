70a319949b9449ccedda02bf9b14181c3cdca275
18326395619 <18326395619@163.com>
2021-02-19 14:54:46 +0800

    将项目初始化成Java项目

---
+++ src/com/bai/chap1/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.bai.chap1;
+
+/**
+ * createTime:2021/2/19 14:50
+ * author:留白
+ * desc:
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
